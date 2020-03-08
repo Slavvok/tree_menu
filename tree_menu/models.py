67d4bffd33987fecb051fb3bbb5bb8ee9fe731ce
Kosovskih Svyatoslav <slavvok@gmail.com>
2020-03-08 20:15:43 +0300

    pep8

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
-    parent = models.ForeignKey("self", related_name='children', null=True, blank=True, on_delete=models.CASCADE)
+    parent = models.ForeignKey("self", related_name='children',
+                               null=True, blank=True, on_delete=models.CASCADE)
     is_main = models.BooleanField(default=False)
 
     def __str__(self):
