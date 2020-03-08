52c11d0d972f48b32d7abc5a06f52aa9994553df
Kosovskih Svyatoslav <slavvok@gmail.com>
2020-03-08 20:12:40 +0300

    draw_menu empty queryset exception

--- tree_menu/templatetags/tags.py
+++ tree_menu/templatetags/tags.py
@@ -7,7 +7,9 @@ register = template.Library()
 
 @register.inclusion_tag('menu.html', takes_context=True)
 def draw_menu(context, slug):
-    parentmenu = Menu.objects.filter(slug=slug)
+    parentmenu = Menu.objects.filter(slug=slug, is_main=True)
+    if not parentmenu.exists():
+        raise Exception(f"No such main menu: {slug}")
     return {'menus': parentmenu, 'request': context['request']}
 
 
